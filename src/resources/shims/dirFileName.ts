f8f1cf0549368f3c356e80426f6a71410df56313
jason lim <50891910+Sxxov@users.noreply.github.com>
2021-08-13 00:12:38 +0800

    Don't apply shim if running in commonjs environment

--- src/resources/shims/dirFileName.ts
+++ src/resources/shims/dirFileName.ts
@@ -1,13 +1,16 @@
 import { fileURLToPath } from 'url';
 import * as pathTool from 'path';
 
-Object.defineProperties(globalThis, {
-	__filename: {
-		get: () => fileURLToPath(import.meta.url),
-		enumerable: true,
-	},
-	__dirname: {
-		get: () => pathTool.dirname(fileURLToPath(import.meta.url)),
-		enumerable: true,
-	},
-});
+if (globalThis.__dirname == null
+	&& globalThis.__filename == null) {
+	Object.defineProperties(globalThis, {
+		__filename: {
+			get: () => fileURLToPath(import.meta.url),
+			enumerable: true,
+		},
+		__dirname: {
+			get: () => pathTool.dirname(fileURLToPath(import.meta.url)),
+			enumerable: true,
+		},
+	});
+}
